3300cb66d1dc60571d0d26088ec5ae9673c65d9e
tungtrieu0801 <trieutungvp@gmail.com>
2026-04-20 22:02:37 +0700

    fix

--- config/db.js
+++ config/db.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.DB_HOST}/${process.env.DB_NAME}`, {
-            auth: {
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-            },
-            authSource: 'admin',
-        });
+        await mongoose.connect(process.env.DB_URI);
+
         console.log('✅ MongoDB connected');
     } catch (err) {
         console.error('❌ MongoDB connection error:', err);
